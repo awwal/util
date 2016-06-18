1e51eef38ed9e6462b5c9a247d799e06becab5e4
Lawal Olufowobi <talktolawal@gmail.com>
2016-06-19 02:53:56 +0300

    Update ubuntu-provision.sh

--- ubuntu-provision.sh
+++ ubuntu-provision.sh
@@ -2,4 +2,15 @@
 sudo apt-get install -y vim curl git python-software-properties maven unzip
 # Install Zsh shell
 sudo apt-get update && sudo apt-get install zsh 
+wget –no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O – | sh 
 
+# Make ZSH default shell
+chsh -s /bin/zsh
+
+# install nice fonts
+wget -O powerline-fonts.zip https://github.com/powerline/fonts/archive/master.zip
+unzip powerline-fonts.zip
+bash ./fonts-master/install.sh
+
+rm -rf powerline-fonts
+rm -f powerline-fonts.zip
