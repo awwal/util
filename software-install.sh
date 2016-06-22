b77ff5679488540cf781789a65f814142e130265
Lawal Olufowobi <talktolawal@gmail.com>
2016-06-22 15:04:37 +0300

    Update software-install.sh

--- software-install.sh
+++ software-install.sh
@@ -3,3 +3,8 @@ sudo apt-get install -y guake
  
 #auto start guake sh -c "sleep 30s; guake"
 cp guake.desktop ~/.config/autostart
+
+#Automount ntfs partition with UUID (get uuid from 'sudo blkid')
+#gksudo gedit /etc/fstab
+#UUID=307FCFC34053E715 /media/ntfs ntfs rw,nosuid,nodev,noatime,allow_other 0 0
+
