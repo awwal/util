394999e55f6a9f02e5f88ce3f8f1a7642abf9911
Lawal Olufowobi <talktolawal@gmail.com>
2016-06-22 10:52:08 +0300

    Update software-install.sh

--- software-install.sh
+++ software-install.sh
@@ -1,3 +1,5 @@
- sudo apt-get install -y guake
+#!/bin/bash
+sudo apt-get install -y guake
  
-#auto start guake sh -c "sleep 120s; guake"
+#auto start guake sh -c "sleep 30s; guake"
+cp guake.desktop ~/.config/autostart
