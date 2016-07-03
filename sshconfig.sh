1810764d4490c0c0aecb91c7cc72dd6597cb9ccc
Lawal Olufowobi <talktolawal@gmail.com>
2016-07-03 12:10:02 +0300

    Create sshconfig.sh

---
+++ sshconfig.sh
@@ -0,0 +1,7 @@
+
+#!/bin/bash 
+
+#create shortcut. With below you can do 'ssh work'
+touch ~/.ssh/config
+chmod 600 ~/.ssh/config
+echo -e "Host work\nHostName my.workserver.com\nUser myuser\n" \ >> ~/.ssh/config
