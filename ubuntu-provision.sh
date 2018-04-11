9333f7fd5311ae45857afcbbe8581d0386129e80
Lawal Olufowobi <talktolawal@gmail.com>
2018-04-11 09:41:10 +0300

    Update ubuntu-provision.sh

--- ubuntu-provision.sh
+++ ubuntu-provision.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get install -y openjdk-8-jdk
 sudo apt-get install -y vim curl git python-software-properties maven unzip 
-sudo apt-get -y install python-pip
+sudo apt-get -y install python-pip libxml2-utils
 
 # Install Zsh shell
 sudo apt-get update && sudo apt-get install -y zsh 
